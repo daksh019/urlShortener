e15d2005376c068f57d77ede178dceb5def1194f
chetan <chetansharma.019@gmail.com>
2021-04-19 13:14:58 +0200

    remove unrequired controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UrlModule } from '@url-svcs/url/url.module';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://localhost/urls'), UrlModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
